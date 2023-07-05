8c02e1d3cf9085f30bfb962c2c05a0e509d3e450
JohnLu2004 <john.lu10212004@gmail.com>
2023-07-05 11:02:46 -0400

    courses page now has 2 placeholder courses ready

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,9 +1,16 @@
 import '../styles/styles.css'
 
+import CourseCard from '../components/CourseCard';
+
 export default function Courses() {
   return (
     <>
-      <h1>List will be here soon</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="w-full p-96">
+          <CourseCard name="MATH1007" definition="First year calc class"/>
+          <CourseCard name="COMP1405" definition="First year CS class"/>
+        </div>
+      </div>
     </>
   );
 }
