079ead9425cc064ad8d0552cf20a3277a1dd2d69
JohnLu2004 <john.lu10212004@gmail.com>
2023-07-05 22:46:12 -0400

    cropped my photo and shortened bios
    still need to fix Ben's

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -5,25 +5,23 @@ import Filters from '../components/Filters'
 export default function Search() {
   return (
     <>
-      <div className='backdrop-blur-sm'>
-        <div className="flex flex-col items-center pt-60 min-h-screen">
-          <div className="">
-            <h1 className="text-blue-400 font-bold text-center flex self-end text-5xl m-auto">CarletonU Courses</h1>
-          </div>
-          <div className="p-2">
-            <div className='flex self-start m-auto'>
-              <input 
-              type="search" 
-              id="default-search" 
-              className="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-              placeholder="Enter a course..."
-            />
-            <NavLink className='block w-1/3 rounded-lg bg-indigo-500' to="courses">Search</NavLink>
+      <div className="flex flex-col items-center pt-60 min-h-screen">
+        <div className="">
+          <h1 className="text-blue-400 font-bold text-center flex self-end text-5xl m-auto">CarletonU Courses</h1>
+        </div>
+        <div className="p-2">
+          <div className='flex self-start m-auto'>
+            <input 
+            type="search" 
+            id="default-search" 
+            className="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Enter a course..."
+          />
+          <NavLink className='block w-1/3 rounded-lg bg-indigo-500' to="courses">Search</NavLink>
           </div>
         </div>
         <Filters/>
       </div>
-    </div>
     </>
   )
 }
