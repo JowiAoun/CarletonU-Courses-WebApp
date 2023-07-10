67db4a7597266f6c589105e7f4c1a0f86f033a0b
JohnLu2004 <john.lu10212004@gmail.com>
2023-07-10 15:46:55 -0400

    just used prettier to indent properly

--- frontend/src/components/FilterContext.jsx
+++ frontend/src/components/FilterContext.jsx
@@ -1,19 +1,19 @@
 // FilterContext.js
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
 export const FilterContext = createContext();
 
 export const FilterProvider = ({ children }) => {
   const [inputValue, setInputValue] = useState({
-    inPerson:false,
-    online:false,
-    summer:false,
-    fall:false,
-    winter:false,
-    firstYear:false,
-    secondYear:false,
-    thirdYear:false,
-    fourthYear:false,
+    inPerson: false,
+    online: false,
+    summer: false,
+    fall: false,
+    winter: false,
+    firstYear: false,
+    secondYear: false,
+    thirdYear: false,
+    fourthYear: false,
   });
 
   const handleInputChange = (value) => {
--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,28 +1,30 @@
-import '../styles/styles.css'
-import Filters from '../components/Filters'
+import "../styles/styles.css";
+import Filters from "../components/Filters";
 
-function Title(){
-  return(
+function Title() {
+  return (
     <div className="">
-      <h1 className="text-blue-400 font-bold text-center flex self-end text-5xl m-auto">CarletonU Courses</h1>
+      <h1 className="text-blue-400 font-bold text-center flex self-end text-5xl m-auto">
+        CarletonU Courses
+      </h1>
     </div>
-  )
+  );
 }
 
-function SearchBarAndButton(){
-  return(
+function SearchBarAndButton() {
+  return (
     <div className="p-2">
-        <div className='flex self-start m-auto'>
-          <input 
-          type="search" 
+      <div className="flex self-start m-auto">
+        <input
+          type="search"
           name="searchBar"
-          id="searchBar" 
-          className="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-          />
-        <button className='block w-1/3 rounded-lg bg-indigo-500'>Search</button>
-        </div>
+          id="searchBar"
+          className="block w-full p-4 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+        <button className="block w-1/3 rounded-lg bg-indigo-500">Search</button>
+      </div>
     </div>
-  )
+  );
 }
 
 function submitForm(event) {
@@ -51,28 +53,32 @@ function submitForm(event) {
 
   //new version
   // Make an API request using fetch or XMLHttpRequest
-  fetch('http://localhost:5000/api/search', {
-    method: 'POST',
-    body: formData
-  }).then(response => response.text())
-  .then(data => {
-    // Access the response data here
-    console.log(data); // "success"
+  fetch("http://localhost:5000/api/search", {
+    method: "POST",
+    body: formData,
   })
-  .catch(error => {
-    // Handle any errors that occur during the request
-    console.error(error);
-  });
+    .then((response) => response.text())
+    .then((data) => {
+      // Access the response data here
+      console.log(data); // "success"
+    })
+    .catch((error) => {
+      // Handle any errors that occur during the request
+      console.error(error);
+    });
 }
 
 export default function SearchPage() {
   return (
     <div>
-      <form className="flex flex-col items-center pt-60 min-h-screen" onSubmit={submitForm}>
+      <form
+        className="flex flex-col items-center pt-60 min-h-screen"
+        onSubmit={submitForm}
+      >
         <Title />
         <SearchBarAndButton />
-        <Filters/>
+        <Filters />
       </form>
     </div>
-  )
+  );
 }
