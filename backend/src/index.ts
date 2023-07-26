6645ea95f115f007152f9b949cc9683b8f789e67
JohnLu2004 <john.lu10212004@gmail.com>
2023-07-26 01:28:38 -0400

    Revert "got rid of my backend"
    This reverts commit 17e2a33498137108cba7e2df398bcdd854d5eba0.

---
+++ backend/src/index.ts
@@ -0,0 +1,86 @@
+// --- Imports
+// Modules
+import cors from "cors";
+import { config } from "dotenv";
+import express, { Request, Response } from "express";
+import mongoose, { Mongoose } from "mongoose";
+// Files
+import { searchCourses } from "./functions";
+import Course from "./types";
+
+// --- App
+const app = express(); // Create app object
+config(); // Read .env file
+
+// Constants
+const mongodbURI: string = process.env.MONGODB_URI!;
+
+// --- Database
+//const db: Mongoose = await mongoose.connect(mongodbURI); // Connect to MongoDB cluster
+
+// --- Middleware
+app.use(cors({ origin: "*" })); // Allow cross-origin requests
+app.use(express.json()); // Parse JSON from requests
+
+// --- Endpoints
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/signup", (req: Request, res: Response) => {
+  res.send("Signup page!");
+});
+
+app.get("/login", (req: Request, res: Response) => {
+  res.send("Login page!");
+});
+
+app.get("/home", (req: Request, res: Response) => {
+  res.send("Home page!");
+});
+
+app.get("/settings", (req: Request, res: Response) => {
+  res.send("Settings page!");
+});
+
+app.post("/api/search", (req: Request, res: Response) => {
+  const formData = req.body;
+  const optionSummer = formData.Summer === "Summer";
+  const optionFall = formData.Fall === "Fall";
+  const optionWinter = formData.Winter === "Winter";
+  const optionInPerson = formData.inPerson === "inPerson";
+  const optionOnline = formData.online === "online";
+  const optionFirstYear = formData.firstYear === "firstYear";
+  const optionSecondYear = formData.secondYear === "secondYear";
+  const optionThirdYear = formData.thirdYear === "thirdYear";
+  const optionFourthYear = formData.fourthYear === "fourthYear";
+
+  console.log("Summer: " + optionSummer);
+  res.send("success");
+
+  //TODO: Find a way to not hard-code search settings
+  /*
+  const queryParams = req.query;
+
+  let code: string = queryParams.code as string;
+  let term: string | undefined = (queryParams.term as string) || undefined;
+  let year_standing: string | undefined =
+    (queryParams.year_standing as string) || undefined;
+  let section_type: string | undefined =
+    (queryParams.section_type as string) || undefined;
+
+  const criteria = {
+    code,
+    term,
+    year_standing,
+    section_type,
+  };
+
+  const results = searchCourses(criteria);
+
+  res.send(results);
+  */
+});
+
+// --- Expose port
+app.listen(5000);
