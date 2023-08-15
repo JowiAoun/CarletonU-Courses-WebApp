8fde9ad107294acd5b88d5830fc56e97563060bf
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-08-15 11:14:18 -0400

    Removed unnecessary endpoints
    Endpoints are currently managed by React Router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,6 @@ import express, { Request, Response } from "express";
 import mongoose, { Mongoose } from "mongoose";
 import graphqlHTTP from 'express-graphql'
 // Files
-import { findCoursesByCode } from "./functions";
 import { schema, root } from "./graphql";
 
 // --- Config
@@ -26,56 +25,12 @@ mongoose.connect(MONGODB_URI); // Connect to MongoDB cluster
 app.use(cors({ origin: "*" })); // Allow cross-origin requests
 app.use(express.json()); // Parse JSON from requests
 
-// --- GraphQL
+// --- GraphQL endpoint
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true, //TODO: See if this needs to be turned off
 }));
-
-// --- Endpoints
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-
-app.get("/signup", (req: Request, res: Response) => {
-  res.send("Signup page!");
-});
-
-app.get("/login", (req: Request, res: Response) => {
-  res.send("Login page!");
-});
-
-app.get("/home", (req: Request, res: Response) => {
-  res.send("Home page!");
-});
-
-app.get("/settings", (req: Request, res: Response) => {
-  res.send("Settings page!");
-});
-
-//app.post("/api/search", (req: Request, res: Response) => {
-//  //TODO: Find a way to not hard-code search settings
-//  const queryParams = req.query;
-//
-//  let code: string = queryParams.code as string;
-//  let term: string | undefined = (queryParams.term as string) || undefined;
-//  let year_standing: string | undefined =
-//    (queryParams.year_standing as string) || undefined;
-//  let section_type: string | undefined =
-//    (queryParams.section_type as string) || undefined;
-//
-//  const criteria = {
-//    code,
-//    term,
-//    year_standing,
-//    section_type,
-//  };
-//
-//  const results = findCoursesByCode(criteria);
-//
-//  res.send(results);
-////});
 
 // --- Expose port
 app.listen(PORT); // Listen on port 5000 for requests
